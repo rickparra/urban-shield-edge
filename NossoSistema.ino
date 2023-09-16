e23dfb0d8d6ab7220d3f7411e7f624336961a6e9
Tony Willian <126829960+TonyWillianFIAP@users.noreply.github.com>
2023-09-16 14:43:53 -0300

    Adicionando Wifi

--- NossoSistema.ino
+++ NossoSistema.ino
@@ -1,3 +1,14 @@
+#include "Arduino.h"
+#include "WiFi.h"
+#include <HTTPClient.h>
+
+HTTPClient client;
+char nomeWifi[] = "Wifiteste";
+char senhaWifi[] = "Wifiteste";
+char serverAddress[] = "https://api.tago.io/data";
+char contentHeader[] = "application/json";
+char tokenHeader[]   = "515801e8-e2c8-43a4-9f58-cf4ac4d4b379";
+
 // Defina os pinos para o sensor PIR e os LEDs
 const int pirPin = 2;     // Pino do sensor PIR
 const int redLedPin = 13; // Pino do LED vermelho
@@ -8,6 +19,8 @@ const int sensorPin = A3; // Pino do sensor de luz ambiente
 const int resistorValue = 220; // Valor do resistor em ohms (ajuste conforme necessário)
 
 void setup() {
+  Serial.begin(9600);
+  init_wifi();
   pinMode(pirPin, INPUT); // Configura o pino do sensor PIR como entrada
   pinMode(redLedPin, OUTPUT); // Configura o pino do LED vermelho como saída
   pinMode(greenLedPin, OUTPUT); // Configura o pino do LED verde como saída
@@ -43,4 +56,17 @@ void loop() {
   Serial.println(sensorValue);
 
   delay(1000); // Pequeno atraso para evitar leituras repetidas
+}
+
+void init_wifi() {
+  Serial.println("Conectando WiFi");
+  Serial.print("Wifi: ");
+  Serial.println(nomeWifi);
+  WiFi.begin(nomeWifi, senhaWifi);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+  }
+  Serial.println("WiFi Conectado");
+  Serial.print("Meu IP eh: ");
+  Serial.println(WiFi.localIP());
 }
